71597c5fad69219795311dcd7b54266cbe202c9d
DMJC <jcarthew@gmail.com>
2026-04-13 17:24:16 +1000

    Dock: ignore SIGCHLD to prevent zombie app-launch children

--- Dock/main.m
+++ Dock/main.m
@@ -1,8 +1,14 @@
 #import <AppKit/AppKit.h>
 #import "AppDelegate.h"
+#include <signal.h>
 
 int main(int argc, const char *argv[])
 {
+    /* NSWorkspace may spawn child processes when launching apps from the dock.
+     * Ignore SIGCHLD so exited launcher children are auto-reaped and cannot
+     * accumulate as zombies under AmbrosiaDock. */
+    signal(SIGCHLD, SIG_IGN);
+
     @autoreleasepool {
         NSApplication *app = [NSApplication sharedApplication];
         AppDelegate *delegate = [[AppDelegate alloc] init];
